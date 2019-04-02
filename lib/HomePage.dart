2df9f64e8f929bb41c5ec54e66da51924a5b08a1
Yazdani <yaz4dani@gmail.com>
2019-04-02 13:46:30 +0600

    Second Commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return new Scaffold(
       
-      backgroundColor: Color(0x1A1A23),
+      backgroundColor: Color(0xFF1A1A23),
       
       body: new ListView(
         children: <Widget>[
@@ -85,11 +85,75 @@ class _HomeState extends State<Home> {
               ],
             ),
           ),//seond container end
+          new SizedBox(height: 20.0,),
+          
+          new Container(
+            margin: EdgeInsets.all(15.0),
+            height: 100.0,
+            child: new Card(
+              elevation: 20.0,
+              color: Color(0xFF24272F),
+              child: new Container(
+                margin: EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                  children: <Widget>[
+
+                    new Container(
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Text("69",
+                          style: TextStyle(fontSize: 20.0,color: Colors.white),
+                          ),
+                          new SizedBox(height: 10.0,),
+                          new Text("Review",
+                            style: TextStyle(fontSize: 18.0,color: Colors.grey.withOpacity(0.6)),
+                          ),
+                        ],
+                      ),
+                    ),//first container
+
+                    new Container(
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Text("95",
+                            style: TextStyle(fontSize: 20.0,color: Colors.white),
+                          ),
+                          new SizedBox(height: 10.0,),
+                          new Text("Wishlist",
+                            style: TextStyle(fontSize: 18.0,color: Colors.grey.withOpacity(0.6)),
+                          ),
+                        ],
+                      ),
+                    ),//second container
+                    new Container(
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Text("4598",
+                            style: TextStyle(fontSize: 20.0,color: Colors.white),
+                          ),
+                          new SizedBox(height: 10.0,),
+                          new Text("Likes",
+                            style: TextStyle(fontSize: 18.0,color: Colors.grey.withOpacity(0.6)),
+                          ),
+                        ],
+                      ),
+                    ),//second container
+
+
+                  ],
+                ),
+              ),
+            ),
+          ),//second container end
 
           
-          
-          
-          
+
         ],
       ),
       
