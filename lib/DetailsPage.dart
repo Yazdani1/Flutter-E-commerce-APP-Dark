c15aa658c88a760a04d3b0524d7d5d08a32af3f7
Yazdani <yaz4dani@gmail.com>
2019-04-03 23:37:10 +0600

    New page

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -9,11 +9,7 @@ class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Shopping Details"),
-        backgroundColor: Colors.green,
-      
-      ),
+     
     );
   }
 }
